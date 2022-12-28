d4ba459058d09ed5bddfa0813b669622fc8520ae
Marek Zvara <marek724@gmail.com>
2022-12-28 23:22:54 +0100

    Fix observer removal by overriding instance method instead class setup/tearDown

--- NetworkingModule/EssentialFeed/EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
+++ NetworkingModule/EssentialFeed/EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
@@ -8,17 +8,14 @@
 import XCTest
 import EssentialFeed
 
-
-
-
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    override class func setUp() {
+    override func setUp() {
         super.setUp()
         URLProtocolStub.startInterceptingRequests()
     }
     
-    override class func tearDown() {
+    override func tearDown() {
         super.tearDown()
         URLProtocolStub.stopInterceptingRequests()
     }
@@ -58,7 +55,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_suceedsOnHTTPUrlResponseWithData() {
-        let data = Data()
+        let data = anyData()
         let response = httpResponse()
         let receivedValues = resultValues(for: data, response: response, error: nil)
 
@@ -194,20 +191,16 @@ private extension URLSessionHTTPClientTests {
         }
         
         override func startLoading() {
-            guard let stub = Self.stub
-            else {
-                return
-            }
             
-            if let data = stub.data {
+            if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
             
-            if let response = stub.response {
+            if let response = URLProtocolStub.stub?.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
             
-            if let error = stub.error {
+            if let error = URLProtocolStub.stub?.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
             
