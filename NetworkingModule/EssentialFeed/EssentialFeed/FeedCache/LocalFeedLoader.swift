353028dff03ad8eb223349b72c5a9728e8e019a5
Marek Zvara <marek724@gmail.com>
2023-01-18 23:35:54 +0100

    Add SaveResult type alias to protect code from future breaking

--- NetworkingModule/EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ NetworkingModule/EssentialFeed/EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -12,19 +12,21 @@ public final class LocalFeedLoader {
     private let store: FeedStore
     private let currentDate: () -> Date
     
+    public typealias SaveResult = Error?
+    
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
     }
     
-    fileprivate func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+    fileprivate func cache(_ items: [FeedItem], with completion: @escaping (SaveResult) -> Void) {
         self.store.insertItems(items, timestamp: self.currentDate()) { [weak self] error in
             guard self != nil else { return }
             completion(error)
         }
     }
     
-    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+    public func save(_ items: [FeedItem], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] error in
             guard let self = self else { return }
             
--- NetworkingModule/EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ NetworkingModule/EssentialFeed/EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -81,7 +81,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()], completion: {
             receivedResults.append($0)
         })
@@ -96,7 +96,7 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStoreSpy()
         var sut: LocalFeedLoader? = LocalFeedLoader(store: store, currentDate: Date.init)
         
-        var receivedResults = [Error?]()
+        var receivedResults = [LocalFeedLoader.SaveResult]()
         sut?.save([uniqueItem()], completion: {
             receivedResults.append($0)
         })
