20546963dad850f5621cb9945ecf153103490e36
Marek Zvara <marek724@gmail.com>
2023-01-29 20:21:45 +0100

    Move LocalFeedItem to its own file

---
+++ NetworkingModule/EssentialFeed/EssentialFeed/FeedCache/LocalFeedItem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Marek Zvara on 29/01/2023.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
